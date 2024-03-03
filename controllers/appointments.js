6d7ab5052a51b83e7743722d3914d6dda580e32f
Pookan Pattaradanai <datas.pookan@gmail.com>
2024-03-03 14:39:59 +0700

    update

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -43,6 +43,9 @@ exports.getAppointment=async (req,res,next)=>{
         if(!appointment){
             return res.status(404).json({success:false,msg:`No appointment with the id of ${req.params.id}`});
         }
+        if (appointment.user.toString()!==req.user.id && req.user.role!=='admin'){
+            return res.status(401).json({ success: false, msg: `User ${req.user.id} is not authorized to access this appointment` });
+        }
         res.status(200).json({success:true,data:appointment});
     }
     catch(err){
--- routes/appointments.js
+++ routes/appointments.js
@@ -4,7 +4,7 @@ const {getAppointments,getAppointment,addAppointment,updateAppointment,deleteApp
 const router=express.Router({mergeParams:true});
 const{protect,authorize}=require('../middleware/auth');
 
-router.route('/').get(protect,getAppointments).post(protect,authorize('admin','user'),addAppointment);
-router.route('/:id').get(protect,getAppointment).put(protect,authorize('admin','user'),updateAppointment).delete(protect,authorize('admin','user'),deleteAppointment);
+router.route('/').get(protect,authorize('admin','user'),getAppointments).post(protect,authorize('admin','user'),addAppointment);
+router.route('/:id').get(protect,authorize('admin','user'),getAppointment).put(protect,authorize('admin','user'),updateAppointment).delete(protect,authorize('admin','user'),deleteAppointment);
 
 module.exports=router;
